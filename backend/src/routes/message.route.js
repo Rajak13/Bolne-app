6017f9bf9b1c363ac4a2222980172ffdd19e96e4
Rajak13 <queresma05@gmail.com>
2025-09-16 05:53:31 +0545

    Contact Endpoint created

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getAllContacts } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.get('/send', (req,res) => {
-    res.send("Send Message Endpoint")
-})
+router.get("/contacts", protectRoute, getAllContacts);
+// router.get("/chats", getChatPartneers);
+// router.get("/:id", getMessagesByUserId)
+
+// router.get('/send/:id', sendMessage )
 
 router.get('/receive', (res,req) =>{
     res.send("Receive Message Endpoint")
