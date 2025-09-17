fa849ae94d01e5a5bcd9a3241980abdadb145130
Rajak13 <queresma05@gmail.com>
2025-09-17 05:50:14 +0545

    Message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getAllContacts } from '../controllers/message.controller.js';
+import { getAllContacts, getMessagesByUserId, sendMessage, getChatPartners } from '../controllers/message.controller.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 
 const router = express.Router();
 
-router.get("/contacts", protectRoute, getAllContacts);
-// router.get("/chats", getChatPartneers);
-// router.get("/:id", getMessagesByUserId)
+router.use( arcjetProtection, protectRoute);
 
-// router.get('/send/:id', sendMessage )
+router.get("/contacts",  getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId)
+router.post('/send/:id',  sendMessage)
 
 router.get('/receive', (res,req) =>{
     res.send("Receive Message Endpoint")
